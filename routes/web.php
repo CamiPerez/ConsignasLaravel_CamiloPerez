6bf4f5ecf0d4ffead1953baab3997f8bec216bf0
Camilo <caperezgonz@gmail.com>
2020-01-15 17:14:58 -0300

    Cambio de carpetas

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/peliculas', 'MovieController@index');
+Route::get('/actores', 'ActorController@index');
+Route::get('/generos', 'GenreController@index');
+Route::get('/peliculas/{movie}', 'MovieController@show');
+Route::get('/actores/nuevo', 'ActorController@create');
+Route::post('/actores', 'ActorController@store');
+Route::get('/actores/{actor}', 'ActorController@show');
+Route::get('/peliculas/{movie}/edit', 'MovieController@edit');
+Route::patch('/movies/{movie}', 'MovieController@update');
+Route::get('/generos/{genre}', 'GenreController@show');
